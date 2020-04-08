9641d1a24095474647e524d3aeb16e0c259e9c4f
Vitor Campos <vitor.smd14@gmail.com>
2020-04-08 19:03:28 -0300

    Aulas de react  +  redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PrimeiroComponente from './componentes/PrimeiroComp'
 
+const elemento = document.getElementById('root')
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <div>
+        <PrimeiroComponente></PrimeiroComponente>
+    </div>, 
+   elemento
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// const jsx = <h1>Olá React!</h1>
+// ReactDOM.render(
+//     jsx, 
+//    elemento
+// )
+
