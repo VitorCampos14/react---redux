784d28b33aec82dea769d4e4e92ea57d0429702e
Vitor Campos <vitor.smd14@gmail.com>
2020-04-18 19:42:35 -0300

    Aula do dia 18

---
+++ src/componentes/Membro.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props =>
+<div>
+
+  {props.nome}
+  <strong> {props.sobrenome}</strong>
+
+
+</div>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PrimeiroComponente from './componentes/PrimeiroComp'
+// import PrimeiroComponente from './componentes/PrimeiroComp'
+// import {CompA, CompB} from './componentes/DoisComp'
+// import MultiElementos from './componentes/MultiElementos'
+// import FamiliaSilva from './componentes/FamiliaSilva'
+import Familia from './componentes/Familia'
+import Membro from './componentes/Membro'
 
 const elemento = document.getElementById('root')
 ReactDOM.render(
     <div>
-        <PrimeiroComponente valor = "Buenos Días!" valorII = "Ohayo!"/>
+        <Familia sobrenome ="Pereira">
+            <Membro nome ="Andre"></Membro>
+            <Membro nome ="Vitor"></Membro>
+        </Familia>
+        {/* <FamiliaSilva>
+
+        </FamiliaSilva> */}
+        {/* <MultiElementos></MultiElementos> */}
+        {/* <CompA valor = "Olá!">  </CompA>
+        <CompB valor = "B na área!">  </CompB> */}
+        {/* {<PrimeiroComponente valor = "Buenos Días!" valorII = "Ohayo!"/>} */}
     </div>, 
    elemento
 )
@@ -14,5 +29,5 @@ ReactDOM.render(
 // ReactDOM.render(
 //     jsx, 
 //    elemento
-// )
+// ) 
 
